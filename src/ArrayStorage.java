2e4fa18aee2d71578381eb26827de30719feb159
denisshchurin <denisshchurin@gmail.com>
2018-08-01 21:15:38 +0300

    исправил замечания №2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -7,26 +7,15 @@ public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
     private int size = 0;
 
-    public int getSize() {
-        return size;
-    }
-
     void clear() {
         Arrays.fill(storage, 0, size, null);
     }
 
     void save(Resume resume) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = resume;
-                size += 1;
-                break;
-            }
-        }
+        storage[size++] = resume;
     }
 
     Resume get(String uuid) {
-//        int size = size();
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].uuid)) {
                 return storage[i];
@@ -36,26 +25,18 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
+        int index = -1;
 
-        int number = 0;
-        boolean presence = false;
-//        int size = size();
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].uuid)) {
-                number = i;
-                presence = true;
+                index = i;
             }
         }
 
-        if (presence) {
-//            for (int i = number; i < size; i++) {
-//                storage[i] = storage[i + 1];
-//            }
-            Resume[] tempArray = new Resume[size - 1];
-            System.arraycopy(storage, 0, tempArray, 0, number);
-            System.arraycopy(storage, number + 1, tempArray, number, size - number - 1);
-            storage = tempArray;
-            size -= 1;
+        int numMoved = size - index - 1;
+        if (index >= 0) {
+            System.arraycopy(storage, index + 1, storage, index, numMoved);
+            storage[--size] = null;
         }
     }
 
@@ -66,13 +47,7 @@ public class ArrayStorage {
         return Arrays.copyOf(storage, size);
     }
 
-//    int size() {
-//        int size = 0;
-//        for (int i = 0; i < storage.length; i++) {
-//            if (storage[i] != null) {
-//                size += 1;
-//            } else break;
-//        }
-//        return size;
-//    }
+    int size() {
+        return size;
+    }
 }
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Test for your ArrayStorage implementation
  */
@@ -17,7 +19,7 @@ public class MainTestArrayStorage {
         ARRAY_STORAGE.save(r3);
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.uuid));
-        System.out.println("Size: " + ARRAY_STORAGE.getSize());
+        System.out.println("Size: " + ARRAY_STORAGE.size());
 
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
@@ -27,7 +29,7 @@ public class MainTestArrayStorage {
         ARRAY_STORAGE.clear();
         printAll();
 
-        System.out.println("Size: " + ARRAY_STORAGE.getSize());
+        System.out.println("Size: " + ARRAY_STORAGE.size());
     }
 
     static void printAll() {
