91a1da872be237373ee264962eeb8944b1f52183
denisshchurin <denisshchurin@gmail.com>
2018-08-03 15:00:19 +0300

    added two overloaded methods scheck() added checks in methods update/delete/get/save deleted doubling in ArrayStorage code

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -12,32 +12,63 @@ public class ArrayStorage {
     }
 
     public void save(Resume resume) {
-        storage[size++] = resume;
+        if (!check(resume) && size != storage.length) {
+            storage[size++] = resume;
+        } else {
+            System.out.println("resume is already exists");
+        }
     }
 
     public Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (uuid.equals(storage[i].uuid)) {
-                return storage[i];
+        Resume resume;
+
+        if (check(uuid)) {
+            for (int i = 0; i < size; i++) {
+                if (uuid.equals(storage[i].uuid)) {
+                    resume = storage[i];
+                }
             }
+        } else {
+            System.out.println("resume doesn`t exist");
         }
         return null;
     }
 
     public void delete(String uuid) {
-        int index = -1;
+        if (check(uuid)) {
+            int index = -1;
 
-        for (int i = 0; i < size; i++) {
-            if (uuid.equals(storage[i].uuid)) {
-                index = i;
+            for (int i = 0; i < size; i++) {
+                if (uuid.equals(storage[i].uuid)) {
+                    index = i;
+                }
             }
+
+            int numMoved = size - index - 1;
+            if (index >= 0) {
+                System.arraycopy(storage, index + 1, storage, index, numMoved);
+                storage[--size] = null;
+            }
+        } else {
+            System.out.println("resume doesn`t exist");
+        }
+    }
+
+    public void update(Resume resume) {
+        if (check(resume)) {
+            int index = -1;
+
+            for (int i = 0; i < size; i++) {
+                if (resume.uuid.equals(storage[i].uuid)) {
+                    index = i;
+                }
+                storage[index] = resume;
+            }
+
+        } else {
+            System.out.println("resume doesn`t exist");
         }
 
-        int numMoved = size - index - 1;
-        if (index >= 0) {
-            System.arraycopy(storage, index + 1, storage, index, numMoved);
-            storage[--size] = null;
-        }
     }
 
     /**
@@ -49,5 +80,25 @@ public class ArrayStorage {
 
     public int size() {
         return size;
+    }
+
+    boolean check(Resume resume) {
+        boolean check = false;
+        for (int i = 0; i < size; i++) {
+            if (resume.uuid.equals(storage[i].uuid)) {
+                check = true;
+            }
+        }
+        return check;
+    }
+
+    boolean check(String uuid) {
+        boolean check = false;
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                check = true;
+            }
+        }
+        return check;
     }
 }
