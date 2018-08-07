4d22cca96e9e4578b1c628ebe529c5303a826f7e
denisshchurin <denisshchurin@gmail.com>
2018-08-07 21:32:16 +0300

    introduce interface

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -7,8 +7,9 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+public class ArrayStorage implements Storage {
+    private static final int STORAGE_LIMIT = 10000;
+    private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size;
 
     public void clear() {
@@ -18,7 +19,7 @@ public class ArrayStorage {
 
     public void save(Resume resume) {
         int index = getIndex(resume.getUuid());
-        if (size == storage.length) {
+        if (size == STORAGE_LIMIT) {
             System.out.println("Storage overflow");
         } else if (index >= 0){
             System.out.println("Resume is already exists");
