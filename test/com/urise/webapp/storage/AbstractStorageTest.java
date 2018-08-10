ba1edf57fd169d92dbc01234f186cb81596fa798
denisshchurin <denisshchurin@gmail.com>
2018-08-10 08:43:04 +0300

    did smth

--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -6,8 +6,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public abstract class AbstractStorageTest {
     private Storage storage;
 
@@ -18,13 +16,16 @@ public abstract class AbstractStorageTest {
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
+    private static final Resume resume1 = new Resume(UUID_1);
+    private static final Resume resume2 = new Resume(UUID_2);
+    private static final Resume resume3 = new Resume(UUID_3);
 
     @Before
     public void setUp() throws Exception {
         storage.clear();
-        storage.save(new Resume(UUID_1));
-        storage.save(new Resume(UUID_2));
-        storage.save(new Resume(UUID_3));
+        storage.save(resume1);
+        storage.save(resume2);
+        storage.save(resume3);
     }
 
     @Test
@@ -34,17 +35,20 @@ public abstract class AbstractStorageTest {
 
     @Test
     public void clear() throws Exception {
-
+        storage.clear();
+        Assert.assertEquals(0, storage.size());
     }
 
     @Test
     public void update() throws Exception {
-
+        storage.update(resume1);
+        Assert.assertNotSame(resume1, resume1);
     }
 
     @Test
     public void getAll() throws Exception {
-
+        Resume[] storageTest = {resume1, resume2, resume3};
+        Assert.assertArrayEquals(storageTest, storage.getAll());
     }
 
     @Test
