26e3d3db2ee23b84a7bfe90d66b187b149b218f8
denisshchurin@gmail.com <Oehbyltybc11>
2018-08-22 17:30:36 +0300

    add class AbstractPathStorage

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -47,4 +47,19 @@ public class MainFile {
             }
         }
     }
+
+    public static void printDirectoryDeeply2(File dir, String offset) {
+        File[] files = dir.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    System.out.println(offset + "F: " + file.getName());
+                } else if (file.isDirectory()) {
+                    System.out.println(offset + "D: " + file.getName());
+                    printDirectoryDeeply2(file, offset + "  ");
+                }
+            }
+        }
+    }
 }
